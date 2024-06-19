692e32dd0f148ff3fcab499a16dce6cd285c7c7f
nickzap872 <167074229+nickzap872@users.noreply.github.com>
2024-06-19 18:42:21 -0400

    Remove main and add newHeader with victory text

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Assign an id of 'victory' to the <h1> element
+newHeader.id = 'victory';
+
+// Set the text of the <h1> element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new <h1> element to the body
+document.body.append(newHeader);
